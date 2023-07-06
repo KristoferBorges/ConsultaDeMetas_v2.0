7268103921bd4e9df983894e2b8eef085b83e142
Administrator <kristofer.souza2027@hotmail.com>
2023-07-05 19:26:17 -0800

    Iniciando o processo de alteração dos arquivos.txt para o excel

---
+++ teste.py
@@ -0,0 +1,6 @@
+import pandas as pd
+
+lista_RDMarcas = pd.read_excel('storage/.xlsx', sheet_name='RDMARCAS')
+lista_Perfumaria = pd.read_excel('', sheet_name='PERFUMARIA')
+lista_Dermo = pd.read_excel('', sheet_name='DERMO')
+
