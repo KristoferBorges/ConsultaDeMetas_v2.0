3123b9f2d326999922c3ad430a0f2198bf522ebb
Administrator <kristofer.souza2027@hotmail.com>
2023-07-05 23:51:13 -0800

    Não consegui fazer um jeito de editar a lista usando o Pandas, ainda... deixo isso para amanhã

--- teste.py
+++ teste.py
@@ -1,6 +1,63 @@
 import pandas as pd
+import openpyxl
+from modulo import dateVerification
 
-lista_RDMarcas = pd.read_excel('storage/.xlsx', sheet_name='RDMARCAS')
-lista_Perfumaria = pd.read_excel('', sheet_name='PERFUMARIA')
-lista_Dermo = pd.read_excel('', sheet_name='DERMO')
+# Define as opções de exibição
+pd.set_option('display.max_columns', None)  # Exibe todas as colunas
+pd.set_option('display.max_rows', None)  # Exibe todas as linhas
+pd.set_option('display.width', 1000)  # Largura máxima da exibição
 
+# Lê os DataFrames do arquivo Excel
+df_lista_RDMarcas = pd.read_excel('storage/listaRDMARCAS.xlsx')
+# df_lista_Perfumaria = pd.read_excel('storage/listaPERFUMARIA')
+# df_lista_Dermo = pd.read_excel('storage/listaDERMO.xlsx')
+
+# Soma os valores
+df_lista_RDMarcas['Meta'] = df_lista_RDMarcas['Meta'].astype(str).str.replace('R$', '').replace(',', '.').astype(float)
+metaAC = df_lista_RDMarcas['Meta'].sum().astype(float)
+
+
+df_lista_RDMarcas['Venda'] = df_lista_RDMarcas['Venda'].astype(str).str.replace('R$', '').replace(',', '.').astype(float)
+vendaAC = df_lista_RDMarcas['Venda'].sum().astype(float)
+
+print(metaAC, vendaAC)
+
+data_input = "10/05/2002"
+metaDia_input = "12000.05"
+metaAC = metaAC + float(metaDia_input)
+vendasDia_input = "10000.64"
+vendaAC = vendaAC + float(vendasDia_input)
+
+if vendaAC < metaAC:
+    sobras = metaAC - vendaAC
+    devedor = '-'
+elif metaAC < vendaAC:
+    sobras = vendaAC - metaAC
+else:
+    sobras = 0
+
+porcentagem = (vendaAC / metaAC) * 100
+
+# Cria uma linha de novos dados
+novoDado = pd.DataFrame([[data_input, metaDia_input, metaAC, vendasDia_input, vendaAC, sobras, porcentagem]],
+                        columns=df_lista_RDMarcas.columns)
+
+# Tentativa de formatação
+df_lista_RDMarcas['Meta'] = 'R$' + df_lista_RDMarcas['Meta'].map('{:.2f}'.format)
+df_lista_RDMarcas['Venda'] = 'R$' + df_lista_RDMarcas['Venda'].map('{:.2f}'.format)
+
+if vendaAC < metaAC:
+    sobras = metaAC - vendaAC
+    df_lista_RDMarcas['Sobras'] = df_lista_RDMarcas['Sobras'].apply(lambda x: '-R$' + '{:.2f}'.format(x) if isinstance(x, (int, float)) else x)
+else:
+    df_lista_RDMarcas['Sobras'] = df_lista_RDMarcas['Sobras'].apply(lambda x: 'R$' + '{:.2f}'.format(x) if isinstance(x, (int, float)) else x)
+
+df_lista_RDMarcas['P'] = df_lista_RDMarcas['P'].apply(lambda x: '{:.2f}%'.format(x) if isinstance(x, (int, float)) else x)
+
+
+
+# Concatela com os dados anteriores da lista
+df_lista_RDMarcas = pd.concat([df_lista_RDMarcas, novoDado], ignore_index=True)
+
+# Salva as alterações
+df_lista_RDMarcas.to_excel('storage/listaRDMARCAS.xlsx', index=False)
